357e074ccaa90d3d6f6d2b19e64629d8d5577162
APavlichenko <apavlichenko@croc.ru>
2023-09-13 11:55:59 +0300

    Вынес testcontainer в проперти

--- demo/main/src/test/java/ru/example/main/TransactionalReadOnlyTest.java
+++ demo/main/src/test/java/ru/example/main/TransactionalReadOnlyTest.java
@@ -3,16 +3,11 @@ package ru.example.main;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.CoreMatchers.containsString;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.DynamicPropertyRegistry;
-import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.context.jdbc.Sql;
-import org.testcontainers.containers.PostgreSQLContainer;
 import ru.example.main.service.CarService;
 
 
@@ -22,25 +17,6 @@ import ru.example.main.service.CarService;
  */
 @SpringBootTest
 public class TransactionalReadOnlyTest {
-
-    static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:14-alpine");
-
-    @BeforeAll
-    static void beforeAll() {
-        postgres.start();
-    }
-
-    @AfterAll
-    static void afterAll() {
-        postgres.stop();
-    }
-
-    @DynamicPropertySource
-    static void configureProperties(DynamicPropertyRegistry registry) {
-        registry.add("spring.datasource.url", postgres::getJdbcUrl);
-        registry.add("spring.datasource.username", postgres::getUsername);
-        registry.add("spring.datasource.password", postgres::getPassword);
-    }
 
     private final long CAR_ID = 5L;
 
